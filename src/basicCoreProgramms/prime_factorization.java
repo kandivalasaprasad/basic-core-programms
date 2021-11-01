b4dd661fcbe1563338579cf2dfc5a724dfed3038
kandivalasa prasad <kandivalasa.prasad97@gmail.com>
2021-11-01 21:16:20 +0530

    Added: prime-factorisation

---
+++ src/basicCoreProgramms/prime_factorization.java
@@ -0,0 +1,26 @@
+package basicCoreProgramms;
+
+import java.util.Scanner;
+
+public class prime_factorization {
+	public static void main(String [] args){
+		
+	Scanner s = new Scanner(System.in);
+	System.out.println(" enter a number : ");
+	int n = s.nextInt();
+	
+	for(int i = 2; i< n; i++) {
+        while(n%i == 0) {
+           System.out.println(i+" ");
+           n = n/i;
+        }
+     }
+     if(n >2) {
+        System.out.println(n);
+     }
+	
+		
+		
+	}
+	
+}
