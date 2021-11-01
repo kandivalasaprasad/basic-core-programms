e98aa8f1e73d7cee04c1666157d8553685a8fa43
kandivalasa prasad <kandivalasa.prasad97@gmail.com>
2021-11-01 20:12:44 +0530

    intialied project

---
+++ src/basicCoreProgramms/harmonic_number.java
@@ -0,0 +1,24 @@
+package basicCoreProgramms;
+
+import java.util.Scanner;
+
+public class harmonic_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println(" enter the value of N : ");
+		int N = s.nextInt();
+		int sum = 1/1;
+		if(N != 0)
+		{
+			sum = (1/1 + 1/N);
+			sum++;
+			System.out.println(sum);
+
+		}
+
+	}
+
+}
---
+++ src/basicCoreProgramms/vowel1.java
@@ -0,0 +1,33 @@
+package basicCoreProgramms;
+
+import java.util.Scanner;
+
+public class vowel1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println(" enter a alphabet:  ");
+
+		char c = s.next().charAt(0);
+		
+	/*	if(c[0] =='A' || c[0] == 'a') 
+			System.out.println(" vowel ");
+		else if (c = 'e' || 'E')
+			System.out.println(" vowel ");
+		else if (c = 'i' || 'I')
+			System.out.println(" vowel ");
+		else if (c = 'o' || 'O')
+			System.out.println(" vowel ");
+		else if (c = 'u' || 'U')
+			System.out.println(" vowel ");
+		else 
+			System.out.println(" consonant ");
+		*/
+		
+		
+	
+	}
+
+}
